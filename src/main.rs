bfac8e0b77e8bdda6dc77ddd538bf9f4083d3d53
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2022-11-30 17:42:41 -0600

    Handle concurrent clients test 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::{net::TcpListener, io::{Write, Read}};
+use std::thread;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,18 +11,20 @@ fn main() {
      let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
      for stream in listener.incoming() {
-         match stream {
-             Ok(mut stream) => {
-                 println!("accepted new connection");
-                 loop {
-                    let mut buf: [u8; 1000] = [0; 1000];
-                    stream.read(&mut buf).expect("Couldn't read stream to buffer!");
-                    stream.write(b"+PONG\r\n").expect("Couldn't respond to PING!");
-                 }
-             }
-             Err(e) => {
-                 println!("error: {}", e);
-             }
-         }
+        thread::spawn(|| {
+            match stream {
+                Ok(mut stream) => {
+                    println!("accepted new connection");
+                    loop {
+                       let mut buf: [u8; 1000] = [0; 1000];
+                       stream.read(&mut buf).expect("Couldn't read stream to buffer!");
+                       stream.write(b"+PONG\r\n").expect("Couldn't respond to PING!");
+                    }
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
+            }
+        });
      }
 }
