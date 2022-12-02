a9d7066eb16182435c820cf915a6052d1183ac22
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2022-12-01 20:24:42 -0600

    Implement the ECHO command test 3

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,20 @@ fn main() {
      let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
      for stream in listener.incoming() {
-        thread::spawn(|| {
-            match stream {
-                Ok(mut stream) => {
-                    println!("accepted new connection");
-                    //loop {
-                       let mut buf: [u8; 1000] = [0; 1000];
-                       stream.read(&mut buf).expect("Couldn't read stream to buffer!");
-                       println!("{:?}", buf);
-                       stream.write(b"+PONG\r\n").expect("Couldn't respond to PING!");
-                    //}
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                loop {
+                    thread::spawn(|| {
+                        let mut buf: [u8; 1000] = [0; 1000];
+                        stream.read(&mut buf).expect("Couldn't read stream to buffer!");
+                        println!("{:?}", buf);
+                        stream.write(b"+PONG\r\n").expect("Couldn't respond to PING!");
+                    })
                 }
-                Err(e) => {
-                    println!("error: {}", e);
-                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
             }
         });
      }
